41b4d64ffd3bc5c31864fb0afc9951948e49e423
Ritik Sinha <ritiksinha91@gmail.com>
2020-10-30 17:55:33 +0530

    Rendering Burger by adding ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    return(
-        <div className={ classes.Burger }>
-            <BurgerIngredient type = 'BreadTop'/>
-            <BurgerIngredient type = 'Meat'/>
-            <BurgerIngredient type = 'Cheese'/>
-            <BurgerIngredient type = 'Salad'/>
-            <BurgerIngredient type = 'Bacon'/>
-            <BurgerIngredient type = 'BreadBottom' /> 
+
+    const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
+        return [...Array(props.ingredients[igkey])].map((_, i) => {
+            return <BurgerIngredient key={igkey + i} type={igkey} />
+        })
+    }
+    )
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='BreadTop' />
+            {transformedIngredients}
+            <BurgerIngredient type='BreadBottom' />
         </div>
     );
 };
--- src/containers/Burger Builder/BurgerBuilder.js
+++ src/containers/Burger Builder/BurgerBuilder.js
@@ -1,14 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     // BreadTop: 0,
+        //     // BreadBottom: 0,
+        //     // Meat: 0,
+        //     // Cheese: 0,
+        //     // Salad: 0,
+        //     // Bacon: 0
+        // }
+    };
+
+    state = {
+        ingredients: {
+            Meat: 1,
+            Cheese: 2,
+            Salad: 1,
+            Bacon: 2
+        },
+        purchased: false,
+        totalPrice: 0
+    };
+
     render() {
 
         return (
             <Auxiliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxiliary>
         );
