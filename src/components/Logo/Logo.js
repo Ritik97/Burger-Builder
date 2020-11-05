2f59e73045418cbb01624499b6a67e87c1ea7d98
Ritik Sinha <ritiksinha91@gmail.com>
2020-11-05 11:58:17 +0530

    Added logo to the Toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../assets/burger-logo.png';
+
+import classes from './Logo.css';
+
+/** Instead of mentioning the image path in the src attr of <img />, we had imported it. This is coz
+ * we are working in the src folder here. But neither the source folder nor its sub-folder gets shipped to 
+ * the server. Instead, the Webpack bundler will bundle these files and folder & create a new output folder,
+ * once we publish our app. By importing the img like this, we make the webpack aware that we are using that img.
+ * The Logo variable will then always get the path of the image, no matter where the img gets stored by the webpack.
+ */
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={Logo} alt='Burger-Logo'></img>
+    </div>
+);
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Logo from '../../Logo/Logo'
 import classes from './Toolbar.css';
 
 const toolbar = (props) => (
 
 <header className={classes.Toolbar} >
     <div>Drawer Toggle</div>
-    <div>Logo</div>
+    <Logo />
     <div>Navigation Items</div>
 </header>
 
