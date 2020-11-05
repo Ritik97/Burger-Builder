4cf4bf5b3164a1daacb29938c1f9dcd7d0e0f363
Ritik Sinha <ritiksinha91@gmail.com>
2020-11-05 11:25:27 +0530

    Added Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
 
     <Auxiliary>
-        <div>
-            Toolbar, SideDrawer, BackDrop
-        </div>
+        <Toolbar />
         <main className = {classes.content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+
+<header className={classes.Toolbar} >
+    <div>Drawer Toggle</div>
+    <div>Logo</div>
+    <div>Navigation Items</div>
+</header>
+
+);
+
+export default toolbar;
